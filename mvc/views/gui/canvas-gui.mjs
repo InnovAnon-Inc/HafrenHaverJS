40e7287c7d6b232ca367bbd1f55e110d45cac999
Innovations Anonymous <InnovAnon-Inc@protonmail.com>
2020-01-16 12:05:36 -0600

    initial commit

--- mvc/views/gui/canvas-gui.mjs
+++ mvc/views/gui/canvas-gui.mjs
@@ -19,20 +19,21 @@ export class CanvasGUI extends GUI {
 		
 		var   offsetX    = event.offsetX;
 		var   offsetY    = event.offsetY;
-		console.log('elem     left,  top    => ' + elemLeft           + ', ' + elemTop);
-		console.log('offset   x,     y      => ' + offsetX            + ', ' + offsetY);
+		//console.log('elem     left,  top    => ' + elemLeft           + ', ' + elemTop);
+		//console.log('offset   x,     y      => ' + offsetX            + ', ' + offsetY);
 		const target     = event.target;
 		if (target != canvas) { // 'this' is our HTMLElement
 			offsetX = target.offsetLeft + event.offsetX;
 			offsetY = target.offsetTop  + event.offsetY;
-			console.log('offset   x,     y      => ' + offsetX            + ', ' + offsetY) }
+			//console.log('offset   x,     y      => ' + offsetX            + ', ' + offsetY)
+		}
 		
 		const x = offsetX / canvas.scrollWidth  * canvas.width;
 		const y = offsetY / canvas.scrollHeight * canvas.height;
 		
-		console.log('scroll   width, height => ' + canvas.scrollWidth + ', ' + canvas.scrollHeight);
-		console.log('logical  width, height => ' + canvas.width       + ', ' + canvas.height);
-		console.log('relative x,     y      => ' + x                  + ', ' + y);
+		//console.log('scroll   width, height => ' + canvas.scrollWidth + ', ' + canvas.scrollHeight);
+		//console.log('logical  width, height => ' + canvas.width       + ', ' + canvas.height);
+		//console.log('relative x,     y      => ' + x                  + ', ' + y);
 		
 		this.handleClick(x, y) }
 	redraw() {
