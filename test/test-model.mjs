3d216c22224ddd6f18cf434b779312e95b00cc63
Innovations Anonymous <InnovAnon-Inc@protonmail.com>
2020-01-16 05:45:41 -0600

    initial commit

--- test/test-model.mjs
+++ test/test-model.mjs
@@ -2,13 +2,13 @@
 import { Model } from '/HafrenHaverJS/mvc/model.mjs';
 
 export class TestModel extends Model {
-	//x;
-	//y;
+	#x;
+	#y;
 	constructor(x, y) {
 		super();
-		this.x = x;
-		this.y = y }
-	get x()  { return this.x }
-	get y()  { return this.y }
-	set x(x) { this.x = x    }
-	set y(y) { this.y = y    } }
+		this.#x = x;
+		this.#y = y }
+	get x()  { return this.#x }
+	get y()  { return this.#y }
+	set x(x) { this.#x = x    }
+	set y(y) { this.#y = y    } }
