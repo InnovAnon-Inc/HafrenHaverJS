6fd09ef5c9be58b1e668985bdb7dda6d3d3cebf3
Innovations Anonymous <InnovAnon-Inc@protonmail.com>
2020-01-16 12:03:13 -0600

    initial commit

--- mvc/views/gui/canvas-gui.mjs
+++ mvc/views/gui/canvas-gui.mjs
@@ -19,13 +19,20 @@ export class CanvasGUI extends GUI {
 		
 		var   offsetX    = event.offsetX;
 		var   offsetY    = event.offsetY;
+		console.log('elem     left,  top    => ' + elemLeft           + ', ' + elemTop);
+		console.log('offset   x,     y      => ' + offsetX            + ', ' + offsetY);
 		const target     = event.target;
 		if (target != canvas) { // 'this' is our HTMLElement
 			offsetX = target.offsetLeft + event.offsetX;
-			offsetY = target.offsetTop  + event.offsetY }
+			offsetY = target.offsetTop  + event.offsetY;
+			console.log('offset   x,     y      => ' + offsetX            + ', ' + offsetY) }
 		
 		const x = offsetX / canvas.scrollWidth;
 		const y = offsetY / canvas.scrollHeight;
+		
+		console.log('scroll   width, height => ' + canvas.scrollWidth + ', ' + canvas.scrollHeight);
+		console.log('logical  width, height => ' + canvas.width + ', ' + canvas.height);
+		console.log('relative x,     y      => ' + x                  + ', ' + y);
 		
 		this.handleClick(x, y) }
 	redraw() {
