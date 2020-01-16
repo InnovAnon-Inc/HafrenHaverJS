7b3ff04262a9de0c13d3edc92393caa45ddf715c
Innovations Anonymous <InnovAnon-Inc@protonmail.com>
2020-01-16 05:10:45 -0600

    initial commit

--- test/test-mvc.mjs
+++ test/test-mvc.mjs
@@ -5,9 +5,9 @@ import { TestController } from '/HafrenHaverJS/test/test-controller';
 import { MVC            } from '/HafrenHaverJS/mvc/mvc';
 
 export function createMVC(id) {
-	Model      m = new TestModel     (0, 0);
-	View       v = new TestGUI       (id);
-	Controller c = new TestController();
-	MVC mvc      = new MVC           (m, v, c);
+	m   = new TestModel     (0, 0);
+	v   = new TestGUI       (id);
+	c   = new TestController();
+	mvc = new MVC           (m, v, c);
 	v.draw();
 	return mvc }
