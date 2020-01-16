b935e774602374ae84bcd1bc1fb9fb0277f5c1d4
Innovations Anonymous <InnovAnon-Inc@protonmail.com>
2020-01-16 10:33:42 -0600

    initial commit

--- test/test-gui.mjs
+++ test/test-gui.mjs
@@ -4,13 +4,14 @@ import { CanvasGUI } from '/HafrenHaverJS/mvc/views/gui/canvas-gui.mjs';
 export class TestGUI extends CanvasGUI {
 	constructor(id) { super(id) }
 	handleClick(x, y) { super.mvc.controller.setXY(x, y) }	
-	draw() {
-		ctx.save();
-		
+	draw() {	
 		const x          = super.mvc.model.x;
 		const y          = super.mvc.model.y;
 		const text       = "(" + x + ", " + y + ")";
 		const ctx        = super.ctx;
+		
+		this.ctx.save();
+		
 		ctx.font         = '8pt Calibri';
 		ctx.textAlign    = 'center';
 		ctx.textBaseline = 'middle';
