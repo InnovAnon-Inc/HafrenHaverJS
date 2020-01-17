7daa7ce5478d1e9dfa76f843d8cdb08f27cf7638
Innovations Anonymous <InnovAnon-Inc@protonmail.com>
2020-01-17 13:16:09 -0600

    solfeggio mvc

--- solfeggio/element/solfeggio-element-model.mjs
+++ solfeggio/element/solfeggio-element-model.mjs
@@ -3,6 +3,7 @@ import { ElementModel } from '/HafrenHaverJS/elemental/element/element-model.mjs
 
 export class SolfeggioElementModel extends ElementModel {
 	constructor(w2, h2, r2, theta, roff, index) {
+		super();
 		this.x     = w2 + r2 * Math.cos(theta);
 		this.y     = h2 + r2 * Math.sin(theta);
 		this.roff  = roff;
