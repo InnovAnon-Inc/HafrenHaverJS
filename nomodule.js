1fc68e0b1a37a48b1029328b2460a597c240669e
Innovations Anonymous <InnovAnon-Inc@protonmail.com>
2020-01-17 13:06:55 -0600

    solfeggio mvc

--- nomodule.js
+++ nomodule.js
@@ -1,6 +1,7 @@
 "use strict";
 
-const main_section = document.getElementById('main-section');
+const main_canvas  = document.getElementById('main-canvas');
+const main_section = main_canvas.parentNode;
 const message      = document.createElement('p');
 message.innerText  = 'JavaScript modules allow for a more featureful experience.';
-main_section.appendChild(message)
+main_section.replaceChild(message, main_canvas)
