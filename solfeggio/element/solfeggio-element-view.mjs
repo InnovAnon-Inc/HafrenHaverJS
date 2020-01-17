50fca4cee9bbd36cc0c26020b458480d5935c416
Innovations Anonymous <InnovAnon-Inc@protonmail.com>
2020-01-17 12:18:02 -0600

    initial commit

--- solfeggio/element/solfeggio-element-view.mjs
+++ solfeggio/element/solfeggio-element-view.mjs
@@ -3,5 +3,23 @@ import { ElementView } from '/HafrenHaverJS/elemental/element/element-view.mjs';
 
 export class SolfeggioElementView extends ElementView {
 	draw() {
-		// TODO
+		this.drawElement();
+		this.drawElementText() }
+	drawElement(x, y, roff2, fillStyle) {
+		const ctx       = this.#ctx;
+		ctx.beginPath();
+		ctx.arc(x, y, roff2, 0, 2 * Math.PI, false);
+		ctx.strokeStyle = "black";
+		ctx.lineWidth   = 1;
+		ctx.stroke();
+		ctx.fillStyle   = fillStyle;
+		ctx.fill();
+	}
+	drawElementText(x, y, fillStyle, text) {
+		const ctx        = this.#ctx;
+		ctx.font         = '8pt Calibri';
+		ctx.textAlign    = 'center';
+		ctx.textBaseline = 'middle';
+		ctx.fillStyle    = fillStyle
+		ctx.fillText(text, x, y);
 	} }
--- solfeggio/solfeggio-gui.mjs
+++ solfeggio/solfeggio-gui.mjs
@@ -2,30 +2,29 @@
 import { CanvasGUI    } from '/HafrenHaverJS/mvc/views/gui/elemental-canvas-gui.mjs';
 import { SOLFEGGIO_DB } from '/HafrenHaverJS/solfeggio/solfeggio-model.mjs';
 
-
-
 export class SolfeggioGUI extends ElementalCanvasGUI {
-	constructor(id) { super(id) }
+	constructor(id) {
+		super(id);
+		
+		this.w2   = this.width  / 2;
+		this.h2   = this.height / 2;
+		const d   = Math.min(w, h);
+		this.r    = d / 2;
+		this.roff = d / 7; // don't go smaller than 7
+	}
 	addElements() {
 		const elements = [];
 		
 		const canvas = this.element;
-		const w      = this.width;
-		const h      = this.height;
-		const w2     = w / 2;
-		const h2     = h / 2;
-		const d      = Math.min(w, h);
 		const len    = SOLFEGGIO_DB.length;
-		const r      = d / 2;
-		const roff   = d / 7; // don't go smaller than 7
-		const roff2  = roff / 2;
-		const r2     = r - roff2;
+		const roff2  = this.roff / 2;
+		const r2     = this.r - roff2;
 		for(var i = 0; i < len; i++) {
 			const theta = i / len * 2 * Math.PI;
-			const x     = w2 + r2 * Math.cos(theta);
-			const y     = h2 + r2 * Math.sin(theta);
+			const x     = this.w2 + r2 * Math.cos(theta);
+			const y     = this.h2 + r2 * Math.sin(theta);
 			const entry = SOLFEGGIO_DB[(i + 8) % len];
-			elements.push([x, y, roff, entry]);
+			elements.push([x, y, this.roff, entry]);
 		}
 		
 		this.elements = Object.freeze(elements) }
@@ -33,27 +32,21 @@ export class SolfeggioGUI extends ElementalCanvasGUI {
 		// TODO get index and set it in model
 		elements.forEach(e => e.controller.handleClick()) }
 	drawBefore() {
-		// TODO draw inner circle
-		// TODO draw outer circle
-	}
-	drawAfter () { /* empty */ }
-	
-	
-	
-	
-	
-	
-	
-	drawOuterCircle(w2, h2, r) {
-		const ctx       = this.#ctx;
+		this.drawOuterCircle();
+		// TODO draw inner circle and polygons
+	}	
+	drawOuterCircle() {
+		const ctx = this.ctx;
 		ctx.beginPath();
-		ctx.arc(w2, h2, r, 0, 2 * Math.PI, false);
+		ctx.arc(this.w2, this.h2, this.r, 0, 2 * Math.PI, false);
 		ctx.strokeStyle = "black";
 		ctx.lineWidth   = 3;
 		ctx.closePath();
 		ctx.stroke();
-		// TODO fill middle background to match selected element's fill style
+		// TODO fill middle background to match selected element's fill style ?
 	}
+	
+	/*
 	drawInnerCircle(w2, h2, r) {
 		const ctx       = this.#ctx;
 		ctx.beginPath();
@@ -64,24 +57,10 @@ export class SolfeggioGUI extends ElementalCanvasGUI {
 		ctx.stroke();
 		// TODO fill inner background to opposite selected element's text
 	}
-	drawElement(x, y, roff2, fillStyle) {
-		const ctx       = this.#ctx;
-		ctx.beginPath();
-		ctx.arc(x, y, roff2, 0, 2 * Math.PI, false);
-		ctx.strokeStyle = "black";
-		ctx.lineWidth   = 1;
-		ctx.stroke();
-		ctx.fillStyle   = fillStyle;
-		ctx.fill();
-	}
-	drawElementText(x, y, fillStyle, text) {
-		const ctx        = this.#ctx;
-		ctx.font         = '8pt Calibri';
-		ctx.textAlign    = 'center';
-		ctx.textBaseline = 'middle';
-		ctx.fillStyle    = fillStyle
-		ctx.fillText(text, x, y);
-	}
+	*/
+	
+	
+	/*
 	drawElements() {
 		const ctx = this.#ctx;
 		const len = this.#elements.length;
@@ -96,45 +75,21 @@ export class SolfeggioGUI extends ElementalCanvasGUI {
 			this.drawElementText(x, y, entry[2],  entry[0]);
 		}
 	}
-	drawArrow (w2, h2, r3, len) {
-		const ctx     = this.#ctx;
+	*/
+	
+	/** draw indicator arrow */
+	drawAfter () {
+		const ctx     = this.ctx;
 		const index2  = this.mvc.model.index;
 		const theta2  = (index2 - 8) / len * 2 * Math.PI;
+		const len     = SOLFEGGIO_DB.length;
 		const entry2  = SOLFEGGIO_DB[index2 % len];
+		const r3      = r - roff;
 
 		ctx.beginPath();
-		ctx.moveTo(w2, h2);
-		ctx.lineTo(w2 + r3 * Math.cos(theta2),  h2 + r3 * Math.sin(theta2));
+		ctx.moveTo(this.w2, this.h2);
+		ctx.lineTo(this.w2 + this.r3 * Math.cos(theta2),  this.h2 + this.r3 * Math.sin(theta2));
 		ctx.strokeStyle = "black"; // entry2[2];
 		ctx.lineWidth   = 3;
 		ctx.closePath();
-		ctx.stroke();
-	}
-	draw() {
-		const canvas = super.element;
-		const ctx    = this.#ctx;
-		//const w      = canvas.scrollWidth;
-		//const h      = canvas.scrollHeight;
-		const w      = canvas.width;
-		const h      = canvas.height;
-		const d      = Math.min(w, h);
-		const w2     = w / 2;
-		const h2     = h / 2;
-		const r      = d / 2;
-		const roff   = d / 7; // don't go smaller than 7
-		const len    = SOLFEGGIO_DB.length;
-		const r3     = r - roff;
-
-		ctx.save();
-		ctx.clearRect(0, 0, w, h);
-
-		// TODO fill outer background to match selected element's text
-
-		this.drawOuterCircle(w2, h2, r);
-		this.drawInnerCircle(w2, h2, r3);
-		this.drawElements();
-		this.drawPolygons();
-		this.drawArrow(w2, h2, r3, len);
-
-		ctx.restore();
-	} }
+		ctx.stroke() } }
